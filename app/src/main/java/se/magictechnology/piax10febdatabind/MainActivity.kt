0749a277992b664326444b0d34a6b1109a339d18
Bill Mårtensson <bill@magictechnology.se>
2022-02-10 11:30:13 +0100

    piax

---
+++ app/src/main/java/se/magictechnology/piax10febdatabind/MainActivity.kt
@@ -0,0 +1,14 @@
+package se.magictechnology.piax10febdatabind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+
+    }
+}
