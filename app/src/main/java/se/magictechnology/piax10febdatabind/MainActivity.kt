20b7b74e497219fa796ebf343a74e7b662a0ea18
Bill Mårtensson <bill@magictechnology.se>
2022-02-10 13:35:01 +0100

    piax

--- app/src/main/java/se/magictechnology/piax10febdatabind/MainActivity.kt
+++ app/src/main/java/se/magictechnology/piax10febdatabind/MainActivity.kt
@@ -2,13 +2,34 @@ package se.magictechnology.piax10febdatabind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableField
+import se.magictechnology.piax10febdatabind.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var fruit = "Banan"
+
+    var thenumber = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
-        
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.sometext = fruit
+
+        binding.somenumber = thenumber
+
+        binding.clickButton.setOnClickListener {
+            fruit = "Kiwi"
+
+            //binding.sometext = "Kiwi"
+
+            thenumber = thenumber + 1
+            binding.somenumber = thenumber
+        }
     }
 }
